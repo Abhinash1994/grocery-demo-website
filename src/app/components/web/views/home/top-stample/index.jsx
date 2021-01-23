6492b12fd1d1fd30024bdef1ceb970e7934aaf8e
Bechu pandit <bechu.pandit@zimyo.com>
2021-01-24 02:18:56 +0530

    dd

--- src/app/components/web/views/home/top-stample/index.jsx
+++ src/app/components/web/views/home/top-stample/index.jsx
@@ -1,7 +1,26 @@
 import React, { Component } from 'react'
 import Slider from "react-slick";
+
+import { Link } from 'react-router-dom';
+import GroceryStampleDetails from '../../../../services/GroceryStampleDetails';
+import CircularProgress from '@material-ui/core/CircularProgress';
+
 export default class Topstample extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            productlist: [],isloaded:false
+        }
+    }
+    async componentDidMount() {
+        let list = await GroceryStampleDetails.getAllGroceryStaple();
+        if(list){
+            this.setState({ productlist: list.data, isloaded:true })
+        }
+    }
     render() {
+        let list = this.state.productlist.products;
+        console.log(list)
         var settings = {
             dots: false,
             infinite: false,
@@ -10,169 +29,85 @@ export default class Topstample extends Component {
             slidesToScroll: 4,
             initialSlide: 0,
             responsive: [
-              {
-                breakpoint: 1024,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                  infinite: true,
-                  dots: false
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: false
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        initialSlide: 3
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                        initialSlide: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
                 }
-              },
-              {
-                breakpoint: 768,
-                settings: {
-                  slidesToShow: 3,
-                  slidesToScroll: 3,
-                  initialSlide: 3
-                }
-              },
-              {
-                breakpoint: 600,
-                settings: {
-                  slidesToShow: 2,
-                  slidesToScroll: 2,
-                  initialSlide: 2
-                }
-              },
-              {
-                breakpoint: 480,
-                settings: {
-                  slidesToShow: 1,
-                  slidesToScroll: 1
-                }
-              }
             ]
-          };
+        };
         return (
             <div>
-                 {/* New Item slider */}
-                 <section className="product-items-slider section-padding">
+                {/* New Item slider */}
+                <section className="product-items-slider section-padding">
                     <div className="container" id="header-category-bk">
                         <div className="section-header">
                             <span>For You</span>
-                            <h5 className="heading-design-h5">Top Stample  {/* <span className="badge badge-primary">20% OFF</span> */}
-                                <a className="float-right text-secondary" >View All</a>
+                            <h5 className="heading-design-h5">Grocery & Staples {/* <span className="badge badge-primary">20% OFF</span> */}
+                                <Link to={{
+                                    pathname: `/shop/${this.state.productlist.slug}`,
+                                    state: list
+                                }}><span className="float-right text-secondary">View All</span></Link>
                             </h5>
                         </div>
                         <Slider {...settings}>
-                            <div className="item">
-                                <div className="product">
-                                    <a href="/p/Oil/9">
-                                        <div className="product-header">
-                                            <span className="badge badge-success">50% OFF</span>
-                                            <img className="img-fluid" src="img/product/nehar-oil.jpeg" alt="product" />
-                                            <span className="veg text-success mdi mdi-circle" />
+                            {!this.state.isloaded ?<div className="progress-bar-bk"><CircularProgress color="secondary" /></div>:
+                                list.map((row, index) => (
+                                    <div key={index} className="item">
+                                        <div className="product">
+                                            <Link to={{
+                                                pathname: `/p/${row.slug}/${row.id}`,
+                                                state: row
+
+                                            }}>
+                                                <div className="product-header">
+                                                    <span className="badge badge-success">{row.discountPer}% OFF</span>
+                                                    <img className="img-fluid" src={row.photo} alt="product" />
+                                                    {/* <span className="veg text-success mdi mdi-circle" /> */}
+                                                </div>
+                                                <div className="product-body">
+                                                    <h5>{row.name}</h5>
+                                                    <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - {row.unitSize}</h6>
+                                                </div>
+                                            </Link>
+                                            <div className="product-footer">
+                                                <button type="button" className="btn btn-secondary btn-sm float-right" onClick={() => this.props.addToCart(row)}><i className="mdi mdi-cart-outline" /> Add To Cart</button>
+                                                <p className="offer-price mb-0">&#x20B9;{row.price} <i className="mdi mdi-tag-outline" /><br /><span className="regular-price">&#x20B9;{row.netPrice}</span></p>
+                                            </div>
                                         </div>
-                                        <div className="product-body">
-                                            <h5>Product Title Here</h5>
-                                            <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - 500 gm</h6>
-                                        </div>
-                                        <div className="product-footer">
-                                            <button type="button" className="btn btn-secondary btn-sm float-right"><i className="mdi mdi-cart-outline" /> Add To Cart</button>
-                                            <p className="offer-price mb-0">$450.99 <i className="mdi mdi-tag-outline" /><br /><span className="regular-price">$800.99</span></p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="item">
-                                <div className="product">
-                                    <a href="/p/Oil/9">
-                                        <div className="product-header">
-                                            <span className="badge badge-success">50% OFF</span>
-                                            <img className="img-fluid" src="img/product/new-product.jpeg" alt="product" />
-                                            <span className="non-veg text-danger mdi mdi-circle" />
-                                        </div>
-                                        <div className="product-body">
-                                            <h5>Product Title Here</h5>
-                                            <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - 500 gm</h6>
-                                        </div>
-                                        <div className="product-footer">
-                                            <button type="button" className="btn btn-secondary btn-sm float-right"><i className="mdi mdi-cart-outline" /> Add To Cart</button>
-                                            <p className="offer-price mb-0">$450.99 <i className="mdi mdi-tag-outline" /><br /><span className="regular-price">$800.99</span></p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="item">
-                                <div className="product">
-                                    <a href="/p/Oil/9">
-                                        <div className="product-header">
-                                            <span className="badge badge-success">50% OFF</span>
-                                            <img className="img-fluid" src="img/product/big-3.jpg" alt="product" />
-                                            <span className="non-veg text-danger mdi mdi-circle" />
-                                        </div>
-                                        <div className="product-body">
-                                            <h5>Product Title Here</h5>
-                                            <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - 500 gm</h6>
-                                        </div>
-                                        <div className="product-footer">
-                                            <button type="button" className="btn btn-secondary btn-sm float-right"><i className="mdi mdi-cart-outline" /> Add To Cart</button>
-                                            <p className="offer-price mb-0">$450.99 <i className="mdi mdi-tag-outline" /><br /><span className="regular-price">$800.99</span></p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="item">
-                                <div className="product">
-                                    <a href="/p/Oil/9">
-                                        <div className="product-header">
-                                            <span className="badge badge-success">50% OFF</span>
-                                            <img className="img-fluid" src="img/product/biscuit.jpeg" alt="product" />
-                                            <span className="veg text-success mdi mdi-circle" />
-                                        </div>
-                                        <div className="product-body">
-                                            <h5>Product Title Here</h5>
-                                            <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - 500 gm</h6>
-                                        </div>
-                                        <div className="product-footer">
-                                            <button type="button" className="btn btn-secondary btn-sm float-right"><i className="mdi mdi-cart-outline" /> Add To Cart</button>
-                                            <p className="offer-price mb-0">$450.99 <i className="mdi mdi-tag-outline" /><br /><span className="regular-price">$800.99</span></p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="item">
-                                <div className="product">
-                                    <a href="/">
-                                        <div className="product-header">
-                                            <span className="badge badge-success">50% OFF</span>
-                                            <img className="img-fluid" src="img/product/img-1.jpg" alt="product" />
-                                            <span className="veg text-success mdi mdi-circle" />
-                                        </div>
-                                        <div className="product-body">
-                                            <h5>Product Title Here</h5>
-                                            <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - 500 gm</h6>
-                                        </div>
-                                        <div className="product-footer">
-                                            <button type="button" className="btn btn-secondary btn-sm float-right"><i className="mdi mdi-cart-outline" /> Add To Cart</button>
-                                            <p className="offer-price mb-0">$450.99 <i className="mdi mdi-tag-outline" /><br /><span className="regular-price">$800.99</span></p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
-                            <div className="item">
-                                <div className="product">
-                                    <a href="/p/Oil/9">
-                                        <div className="product-header">
-                                            <span className="badge badge-success">50% OFF</span>
-                                            <img className="img-fluid" src="img/product/img-2.jpg" alt="product" />
-                                            <span className="veg text-success mdi mdi-circle" />
-                                        </div>
-                                        <div className="product-body">
-                                            <h5>Product Title Here</h5>
-                                            <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - 500 gm</h6>
-                                        </div>
-                                        <div className="product-footer">
-                                            <button type="button" className="btn btn-secondary btn-sm float-right"><i className="mdi mdi-cart-outline" /> Add To Cart</button>
-                                            <p className="offer-price mb-0">$450.99 <i className="mdi mdi-tag-outline" /><br /><span className="regular-price">$800.99</span></p>
-                                        </div>
-                                    </a>
-                                </div>
-                            </div>
+                                    </div>
+                                ))}
                         </Slider>
                     </div>
                 </section>
+
                 {/* End New item slider */}
             </div>
         )
--- src/app/components/web/views/single-product/index.jsx
+++ src/app/components/web/views/single-product/index.jsx
@@ -1,6 +1,9 @@
 import React, { Component } from 'react'
 import { Paper } from '@material-ui/core';
 import Slider from "react-slick";
+import parse from 'html-react-parser';
+import { GetProductDetails } from '../../../services';
+
 import './index.css'
 class Singleproduct extends Component {
     constructor(props) {
@@ -11,9 +14,13 @@ class Singleproduct extends Component {
     }
     async componentDidMount() {
         window.scrollTo(0, 0)
+        let url = window.location.href.split('/');
+        var lastSegment = url.pop() || url.pop();
+        let list = await GetProductDetails.getProductById(lastSegment);
+        this.setState({ product: list.data })
     }
     render() {
-        const { product } = this.state;
+        const{ product } = this.state;
         const settings = {
             customPaging: function (i) {
                 return (
@@ -45,73 +52,79 @@ class Singleproduct extends Component {
 
                 <section className="shop-single section-padding pt-3">
                     <div className="container">
+                        {
+                            product ?
+                                <div className="row">
+                                    <div className="col-md-6">
+                                        <div className="shop-detail-left">
+                                            <Paper className="shop-detail-slider">
+                                                <Slider {...settings}>
+                                                    {
+                                                        product.productphotos ?
+                                                            product.productphotos.map((r, index) => {
+                                                                return (
+                                                                    <div key={index}>
+                                                                        <img alt src={r.imgUrl} className="img-fluid img-center" />
+                                                                    </div>
+                                                                )
+                                                            })
+                                                            : "Please Upload Image"
+                                                    }
 
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="shop-detail-left">
-                                    <Paper className="shop-detail-slider">
-                                        <Slider {...settings}>
-                                            <div >
-                                                <img alt src="/img/product/biscuit.jpeg" className="img-fluid img-center" />
-                                            </div>
+                                                </Slider>
 
-                                        </Slider>
-
-                                    </Paper>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="shop-detail-right">
-                                    <span className="badge badge-success">5% OFF</span>
-                                    <h2>Fortune Sunflower Refined Oil, 5 L Can</h2>
-                                    <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - 5kg</h6>
-                                    <div className="pdp-product__old-price">
-                                        <span className="space__right--2-unit">Product MRP:</span><span className="regular-price">&#x20B9;500</span>
-                                    </div>
-
-                                    <div className="pdp-product__new-price">
-                                        <span className="space__right--2-unit">Selling price:</span>
-                                        <span className="pdp-product__price--new">&#x20B9;500</span>
-                                        <div className="pdp-product__tax-disclaimer">(Inclusive of all taxes)</div>
-                                    </div>
-
-                                    <button type="button" className="btn btn-secondary btn-lg" onClick={() => this.props.addToCart(product)}><i className="mdi mdi-cart-outline" /> Add To Cart</button>
-                                    <h6 className="mb-3 mt-4">Why shop from Groci?</h6>
-                                    <div className="row">
-                                        <div className="col-md-12">
-                                            <div className="feature-box">
-                                                <i className="mdi mdi-truck-fast" />
-                                                <h6 className="text-info"><span>Easy Returns &amp; Refunds</span></h6>
-                                                <p>Return products at doorstep and get refund in seconds.</p>
-                                            </div>
-                                        </div>
-                                        <div className="col-md-12">
-                                            <div className="feature-box">
-                                                <i className="mdi mdi-basket" />
-                                                <h6 className="text-info">Lowest price guaranteed</h6>
-                                                <p>Get difference refunded if you find it cheaper anywhere else.</p>
-                                            </div>
+                                            </Paper>
                                         </div>
                                     </div>
-                                </div>
-                            </div>
+                                    <div className="col-md-6">
+                                        <div className="shop-detail-right">
+                                            <span className="badge badge-success">{product.discountPer}% OFF</span>
+                                            <h2>{product.name}</h2>
+                                            <h6><strong><span className="mdi mdi-approval" /> Available in</strong> - {product.unitSize}</h6>
+                                            <div className="pdp-product__old-price">
+                                                <span className="space__right--2-unit">Product MRP:</span><span className="regular-price">&#x20B9;{product.price}</span>
+                                            </div>
 
-                            <div className="col-lg-12 col-md-12">
-                                <div className="pdpt-bg">
-                                    <div className="pdpt-title">
-                                        <h4>Product Details</h4>
+                                            <div className="pdp-product__new-price">
+                                                <span className="space__right--2-unit">Selling price:</span>
+                                                <span className="pdp-product__price--new">&#x20B9;{product.netPrice}</span>
+                                                <div className="pdp-product__tax-disclaimer">(Inclusive of all taxes)</div>
+                                            </div>
+
+                                            <button type="button" className="btn btn-secondary btn-lg" onClick={() => this.props.addToCart(product)}><i className="mdi mdi-cart-outline" /> Add To Cart</button>
+                                            <h6 className="mb-3 mt-4">Why shop from Groci?</h6>
+                                            <div className="row">
+                                                <div className="col-md-12">
+                                                    <div className="feature-box">
+                                                        <i className="mdi mdi-truck-fast" />
+                                                        <h6 className="text-info"><span>Easy Returns &amp; Refunds</span></h6>
+                                                        <p>Return products at doorstep and get refund in seconds.</p>
+                                                    </div>
+                                                </div>
+                                                <div className="col-md-12">
+                                                    <div className="feature-box">
+                                                        <i className="mdi mdi-basket" />
+                                                        <h6 className="text-info">Lowest price guaranteed</h6>
+                                                        <p>Get difference refunded if you find it cheaper anywhere else.</p>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
                                     </div>
-                                    <div className="pdpt-body scrollstyle_4">
-                                        <div className="pdct-dts-1 short-desc">
-                                            <p>
-                                                What is Lorem Ipsum?
-                                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-                                            </p>
+
+                                    <div className="col-lg-12 col-md-12">
+                                        <div className="pdpt-bg">
+                                            <div className="pdpt-title">
+                                                <h4>Product Details</h4>
+                                            </div>
+                                            <div className="pdpt-body scrollstyle_4">
+                                                <div className="pdct-dts-1 short-desc">
+                                                    {parse(product.desc)}
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
                                 : "Loading"
                         }
 
