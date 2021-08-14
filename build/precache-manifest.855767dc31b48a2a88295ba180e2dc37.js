954d14cabbb413609e57d3576e40faf03a9161e4
mohou <mohoumouhamed01@gmail.com>
2021-08-14 09:06:31 +0000

    feat:online

---
+++ build/precache-manifest.855767dc31b48a2a88295ba180e2dc37.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c67f168770dbea8445af1aac2e53d03",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48404e7d3f2c6407bdb2",
+    "url": "/static/css/2.e62bb353.chunk.css"
+  },
+  {
+    "revision": "9bb7b4258ee512aedc93",
+    "url": "/static/css/main.906be252.chunk.css"
+  },
+  {
+    "revision": "48404e7d3f2c6407bdb2",
+    "url": "/static/js/2.58e13da4.chunk.js"
+  },
+  {
+    "revision": "d3ea72c3d60d6591eeba63b35bff49ff",
+    "url": "/static/js/2.58e13da4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9bb7b4258ee512aedc93",
+    "url": "/static/js/main.52615e59.chunk.js"
+  },
+  {
+    "revision": "8cd2443f0fa8d322457d",
+    "url": "/static/js/runtime-main.7d900dff.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
